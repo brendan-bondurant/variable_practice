6e3edb9a8308a44ea18d00989bc3a483cdaf2018
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-07-30 09:12:41 -0600

    Show three Boolean variables

--- variables.rb
+++ variables.rb
@@ -12,3 +12,8 @@ pets = 2
 deposit_paid_to_turing = 1200.00
 tuition_for_turing = 25000.00
 negative_number = -5
+
+# Boolean
+career_change = true
+ready_for_a_new_challenge = true
+know_everything_about_ruby = false
